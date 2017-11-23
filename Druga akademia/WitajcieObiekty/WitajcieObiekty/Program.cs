36a626926ad99f15e4852c2006f6a02bbc16950e
Michał Chęciński <michal.checinski@outlook.com>
2017-11-23 21:53:15 +0100

    Added examples from second c# academy

---
+++ Druga akademia/WitajcieObiekty/WitajcieObiekty/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WitajcieObiekty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studentPierwszy = new Student(20, "Michał", "Chęciński", 284907);
+            //studentPierwszy.Wiek = 19; //to przypisanie nie zadziała - set właściwości(property) Wiek jest ustawiona na private
+
+            Student studentDrugi = new Student(22, "Filip", "Kapczyński", 282907);
+
+            Console.WriteLine("Dane obiektu studentPierwszy:");
+            studentPierwszy.DaneStudenta();
+            Console.WriteLine(); //wypisuję pustą linię w celu czytelniejszego 
+            Console.WriteLine("Dane obiektu studentDrugi:");
+            studentDrugi.DaneStudenta();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Druga akademia/WitajcieObiekty/WitajcieObiekty/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WitajcieObiekty
+{
+    public class Student
+    {
+        //Tylko z wewnątrz możemy ustawić te propertisy za sprawą słówka kluczowego private przy operacji set
+        public int Wiek { get; private set; }
+        public int NumerIndexu { get; private set; }
+        public string KierunekStudiow { get; private set; }
+        public string Imie { get; private set; }
+        public string Nazwisko { get; private set; }
+
+        public Student(int Wiek, string imie, string nazwisko, int numerIndexu)
+        {
+            this.Wiek = Wiek;
+            Imie = imie;
+            Nazwisko = nazwisko;
+            NumerIndexu = numerIndexu;
+        }
+
+        public void UczSie()
+        {
+            Console.WriteLine("Uczę się!!!!");
+        }
+
+
+        public int ZwiekszWiek()
+        {
+            Wiek += 1; //Wiek = Wiek + 1 Wiek++
+            return Wiek;
+            //return Wiek++; //zamiast dwóch linijek powyżej można tą operację za pomocą tej jednej. W jednej linii najpierw zwiększam wiek o 1 a później zwracam tą nową wartość
+        }
+
+        public void DaneStudenta()
+        {
+            Console.WriteLine("Mój wiek: " + Wiek);
+            Console.WriteLine("Moje imię: " + Imie);
+            Console.WriteLine("Moje nazwisko: " + Nazwisko);
+        }
+
+    }
+}
